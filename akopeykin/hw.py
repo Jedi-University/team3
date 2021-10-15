970b9f51fc3a7b919d6ff313d298ee7ec6ab3f50
Andrey Kopeykin <kopagm@gmail.com>
2021-10-15 14:18:20 +0400

    add ex3

--- akopeykin/hw.py
+++ akopeykin/hw.py
@@ -10,3 +10,13 @@ def exercise_2():
     l = [[1,2,3,4,5,6], [0,0,0,0], ["a", "b", "c"]]
     r = reduce(lambda x,y: x+y, l)
     print(r)
+
+def exercise_3():
+    def super_range(i):
+        n = 0
+        while n < i:
+            yield n
+            n += 1
+
+    for x in super_range(10):
+        print(x)
