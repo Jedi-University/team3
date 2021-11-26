49c5c685f194e9655b3ba21184bb94a0d41a19b7
Andrey Kopeykin <kopagm@gmail.com>
2021-11-26 16:53:45 +0400

    rename method

--- clicks/click_location.py
+++ clicks/click_location.py
@@ -1,13 +1,14 @@
+from datetime import datetime
+
 from loguru import logger
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 
 from app_config import location
-# from api.location import location
 
 
 class ClickLocation(MRJob):
-    
+
     DIRS = ['api']
     FILES = ['app_config.py']
 
@@ -15,31 +16,22 @@ class ClickLocation(MRJob):
         ip = line.split()[2]
         yield (ip, 1)
 
-    def combiner_count_ip(self, ip, counts):
-        yield (ip, sum(counts))
-
-    def reducer_count_ip(self, ip, counts):
-        yield (ip, sum(counts))
+    def reducer_sum(self, key, counts):
+        yield (key, sum(counts))
 
     def mapper_location(self, ip, _):
         loc = location.get(ip)
         yield (loc, 1)
 
-    def combiner_location(self, loc, counts):
-        yield (loc, sum(counts))
-
-    def reducer_location(self, loc, counts):
-        yield (loc, sum(counts))
-
     def steps(self):
-        
+
         return [
             MRStep(mapper=self.mapper_count_ip,
-                   combiner=self.combiner_count_ip,
-                   reducer=self.reducer_count_ip),
+                   combiner=self.reducer_sum,
+                   reducer=self.reducer_sum),
             MRStep(mapper=self.mapper_location,
-                   combiner=self.combiner_location,
-                   reducer=self.reducer_location),
+                   combiner=self.reducer_sum,
+                   reducer=self.reducer_sum),
         ]
 
 
@@ -47,4 +39,9 @@ if __name__ == '__main__':
     logger.add('app.log', rotation='0.2 MB',
                retention=1, enqueue=True, diagnose=True)
     logger.info(f'{"-"*5} start {"-"*5}')
+    start_time = datetime.now()
+
     ClickLocation.run()
+
+    time_delta = datetime.now() - start_time
+    logger.info(f'Total minutes: {time_delta.total_seconds()/60:.2f}')
