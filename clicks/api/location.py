eb6639b7c40cc250f6a82db81211d22a53ca0077
Andrey Kopeykin <kopagm@gmail.com>
2021-11-29 17:27:02 +0400

    change retry

--- clicks/api/location.py
+++ clicks/api/location.py
@@ -1,9 +1,10 @@
-from loguru import logger
 import requests
+from loguru import logger
 from retry import retry
 
+
 class Location:
-    
+
     def __init__(self, url: str, *args, **kwargs):
         self.url = url
         self.session = self.get_session()
@@ -12,14 +13,15 @@ class Location:
         session = requests.Session()
         return session
 
-    @retry(tries=-1, delay=0, max_delay=None, backoff=1, jitter=0, logger=logger)
-    def get_api_response(self, ip, **kwargs) -> requests.Response:
+    # @retry(tries=-1, delay=0, max_delay=None, backoff=1, jitter=0, logger=logger)
+    @retry(tries=2, delay=0, max_delay=None, backoff=1, jitter=0, logger=logger)
+    def get_api_response(self, ip: str) -> str:
         response = self.session.get(self.url, params={'ip': ip})
-        logger.debug(response.text)
+        # logger.debug(response.text)
         result = response.text
         return result
 
+    @logger.catch
     def get(self, ip: str):
         location = self.get_api_response(ip)
         return location
-        # return 'Ru'
